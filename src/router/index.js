241c75df5e274d8a5429129fe809136f5a30db4b
BoonDoPark <BoonDoPark>
2024-04-15 20:38:44 +0900

    login 구현

--- src/router/index.js
+++ src/router/index.js
@@ -1,19 +1,24 @@
 import HostMainView from "@/views/HostMainView.vue";
 import HostingListView from "@/views/HostingListView.vue";
 import { createWebHistory, createRouter } from "vue-router";
+import LoginView from "../views/LoginView";
 
 const routes = [
-    {
-      path: "/hosting",
-      component: HostMainView
-    },
-    {
-      path: "/hosting/listings",
-      component: HostingListView
-    }
-  ];
-  const router = createRouter({
-    history: createWebHistory(),
-    routes,
-  });
-  export default router;
+  {
+    path: "/hosting",
+    component: HostMainView,
+  },
+  {
+    path: "/hosting/listings",
+    component: HostingListView,
+  },
+  {
+    path: "/login",
+    component: LoginView,
+  },
+];
+const router = createRouter({
+  history: createWebHistory(),
+  routes,
+});
+export default router;
